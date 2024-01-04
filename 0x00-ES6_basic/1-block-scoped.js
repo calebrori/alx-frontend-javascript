02bf882e2cee4f01d0b6a1a47aae6907d9daa9ac
calebrori <calebrori@gmail.com>
2024-01-04 23:08:46 +0000

    0x00-ES6_basic

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // Use let to create a block-scoped variable
+    const task = true; // Use let to create a block-scoped variable
     const task2 = false; // Use const for variables that won't be reassigned
 
     // The inner variables task and task2 won't overwrite the outer ones
@@ -11,4 +11,3 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
-
