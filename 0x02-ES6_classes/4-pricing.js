4d847d4835d7e5b7e8ee7e0eadbeabfabaac4cba
calebrori <32507245+calebrori@users.noreply.github.com>
2024-01-05 02:41:27 +0300

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,29 +1,31 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') throw TypeError('amount must be a number');
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be a Currency');
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
