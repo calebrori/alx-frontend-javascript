b592a13849a18f009d2134caeb0e7dc395fbf23d
calebrori <calebrori@gmail.com>
2024-01-05 20:05:53 +0000

    0x01-ES6_promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -3,5 +3,5 @@
 // and USDownload (Promise).
 
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload]);
-  }
+  return Promise.race([chinaDownload, USDownload]);
+}
